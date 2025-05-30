16885ca4f2faa0093fcf30ad9a90124209e6c12c
Roovelo <rnjs8657@naver.com>
2025-05-30 17:53:08 +0900

    백준9063번_대지
    int xmin = Integer.MAX_VALUE;
    int xmax = Integer.MIN_VALUE;
    int ymin = Integer.MAX_VALUE;
    int ymax = Integer.MIN_VALUE;
    
    값 비교를 위한 초기값 세팅 배우기

---
+++ P9063.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P9063 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int xmin = Integer.MAX_VALUE;
+        int xmax = Integer.MIN_VALUE;
+        int ymin = Integer.MAX_VALUE;
+        int ymax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (x < xmin) xmin = x;
+            if (x > xmax) xmax = x;
+            if (y < ymin) ymin = y;
+            if (y > ymax) ymax = y;
+        }
+
+        int area = (xmax - xmin) * (ymax - ymin);
+        bw.write(String.valueOf(area));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
